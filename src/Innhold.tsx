ec6b48e4a3c4f400b5ca5543321efae892baee6e
Kjell Arne Brødreskift <kjellabrodreskift@bekk.no>
2017-10-24 13:21:07 +0200

    Installert nav-frontend moduler, less og lesshint

--- src/Innhold.tsx
+++ src/Innhold.tsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
 import Skjema from './skjema/Skjema';
 import Resultat from './resultat/Resultat';
-import { AppState } from './reducer';
-import { connect } from 'react-redux';
-import { selectErAlleSpormalBesvart } from './svar/svar-selector';
+import {AppState} from './reducer';
+import {connect} from 'react-redux';
+import {selectErAlleSpormalBesvart} from './svar/svar-selector';
 
 interface InnholdProps {
-    erAlleSpormalBesvart: boolean;
+	erAlleSpormalBesvart: boolean;
 }
 
 function Innhold({erAlleSpormalBesvart}: InnholdProps) {
-    return (
-        <div>
-            {erAlleSpormalBesvart ? <Resultat/> : <Skjema/>}
-        </div>
-    );
+	return (
+		erAlleSpormalBesvart ? <Resultat/> : <Skjema/>
+	);
 }
 
 const mapStateToProps = (state: AppState): InnholdProps => ({
-    erAlleSpormalBesvart: selectErAlleSpormalBesvart(state)
+	erAlleSpormalBesvart: selectErAlleSpormalBesvart(state)
 });
 
 export default connect(mapStateToProps)(Innhold);
