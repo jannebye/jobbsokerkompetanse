d4dceffa3d562768bb2d1fcda0c6445ad4083b48
Kjell Arne Brødreskift <kjellabrodreskift@bekk.no>
2017-11-21 17:06:45 +0100

    Merge med master

---
+++ src/utils/createTextBundle.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+function readFile(dir, file) {
+    try {
+        return fs.readFileSync(path.join(dir, file), 'utf8');
+    } catch(e) {
+        console.log('Error:', e.stack);
+    }
+}
+
+function createJson(katalog) {
+    return 'const bundle = ' + JSON.stringify(read(katalog));
+}
+
+const read = (dir) =>
+    fs.readdirSync(dir)
+        .reduce((files, file) =>
+            fs.statSync(path.join(dir, file)).isDirectory() ?
+                files.concat(read(path.join(dir, file))) :
+                files.concat({
+                    id: file.split('_')[0],
+                    text: readFile(dir, file).trim()
+                }), []);
+
+try{
+    fs.writeFileSync('../../build/dist/tekster/bundle.js', createJson('../tekster/sporsmal'));
+}catch (e){
+    console.log("Kunne ikke skrive fil ", e);
+}
+
+console.log(
+    read('../tekster/sporsmal')
+);
+
