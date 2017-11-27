69c425e191f06ea8ae895e7fce4ac0f6a2f681cd
Kjell Arne Brødreskift <kjellabrodreskift@bekk.no>
2017-11-27 13:42:31 +0100

    Fjernet ubrukte filer

--- src/utils/henteBundle.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-
-const bundle = JSON.parse(fs.readFileSync('../../build/dist/tekster/bundle.json', 'utf8'));
-console.log(bundle[0]["cv-spm-01"]);
-console.log(bundle);
