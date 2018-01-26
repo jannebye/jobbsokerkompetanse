d8ece8bfcf7ee07b341549d729f25b5a975b3413
Kjell Arne Brødreskift <kjellabrodreskift@bekk.no>
2018-01-26 10:12:03 +0100

    FO-291. Modifisert ekspanderbart panel.

--- src/resultat/temavisning.tsx
+++ src/resultat/temavisning.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import { RaadModell } from './raad-modell';
-import EkspanderbartPanel from 'nav-frontend-ekspanderbartpanel';
 import { veiviserdata } from '../veiviserdata';
-import { FormattedHTMLMessage } from 'react-intl';
+import { Normaltekst, Undertittel } from 'nav-frontend-typografi';
 
 interface AktivitetModell {
     id: string;
@@ -15,44 +14,73 @@ interface TemaModell {
     refid?: string;
     tittel: string;
     ingress: string;
-    aktiviteter?: [AktivitetModell];
+    aktiviteter?: AktivitetModell[];
 }
 
 interface RaadProps {
     raad: RaadModell;
 }
 
-function TemaVisning({ raad }: RaadProps) {
+function toggleEkspander(e: React.SyntheticEvent<HTMLButtonElement>) {
+    const knapp = (e.target as HTMLElement).closest('.ekspanderbartPanel__hode');
+    const panel = (e.target as HTMLElement).closest('.ekspenderbartPanel');
+    if (panel && knapp) {
+        if (panel.classList.contains('ekspanderbartPanel--lukket')) {
+            panel.classList.remove('ekspanderbartPanel--lukket');
+            panel.classList.add('ekspanderbartPanel--apen');
+            knapp.setAttribute('aria-expanded', 'true');
+        } else {
+            panel.classList.remove('ekspanderbartPanel--apen');
+            panel.classList.add('ekspanderbartPanel--lukket');
+            knapp.setAttribute('aria-expanded', 'false');
+        }
+    }
+}
+
+function TemaVisning({raad}: RaadProps) {
     const understeg = veiviserdata.steg.understeg;
     const temagrupper = understeg.map(u => u.temaer);
     const riktiggruppe = temagrupper.find(t => t.some(tt => tt.id === raad.id)!);
-    let tema: TemaModell;
+
+    let tema: TemaModell = {
+        id: '',
+        refid: '',
+        tittel: '',
+        ingress: '',
+        aktiviteter: [{id: '', tittel: '', innhold: ''}]
+    };
     let aktiviteter: AktivitetModell[] = []; // tslint:disable-line:no-any
     if (riktiggruppe !== undefined) {
-        tema = riktiggruppe.find(t => t.id === raad.id);
+        const riktigtema = riktiggruppe.find(t => t.id === raad.id);
+        if (riktigtema !== undefined) {
+            tema = riktigtema;
+        }
         aktiviteter = riktiggruppe.find(t => t.id === raad.id)!.aktiviteter!;
+        console.log('aktiviteter', aktiviteter);
     }
-    console.log('tema', tema);
+
+    let html_innhold = '';
+    aktiviteter.length !== 0 &&
+    aktiviteter.map(a => (
+        html_innhold += `<p>${a.innhold}</p>`
+    ));
+
     return (
-        <li className="enkelt__tema blokk-xs" key={raad.id}>
-            <EkspanderbartPanel apen={false} tittel={raad.tekst}>
-                <div>
-                    {aktiviteter.length !== 0 &&
-                        aktiviteter.map(aktivitet => (
-                            <div key={aktivitet.id}>
-                                <h1>{tema.tittel}</h1>
-                                <p className="aktivitet">
-                                    <FormattedHTMLMessage
-                                        id={aktivitet.id}
-                                        defaultMessage={
-                                            aktivitet.innhold
-                                        }
-                                    />
-                                </p>
-                            </div>
-                        ))}
-                </div>
-            </EkspanderbartPanel>
+        <li className="enkelt__tema" key={raad.id}>
+            <section className="ekspenderbartPanel ekspanderbartPanel--lukket">
+                <button className="ekspanderbartPanel__hode" aria-expanded="false"
+                        onClick={(event: React.SyntheticEvent<HTMLButtonElement>) => {
+                            toggleEkspander(event)
+                        }}
+                >
+                    <Undertittel tag="h1" className="ekspanderbartPanel__heading">
+                        {tema.tittel}
+                    </Undertittel>
+                    <Normaltekst>{tema.ingress}</Normaltekst>
+                    <span className="ekspanderbartPanel__indikator"/>
+                </button>
+                <div className="ekspanderbartPanel__innhold" dangerouslySetInnerHTML={{__html: html_innhold}}/>
+            </section>
         </li>
     );
 }
