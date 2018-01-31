b69290e944a1c9f1c59cc9b7977e6e1cc1f065bc
Johan Rusvik <johan.rusvik@nav.no>
2018-01-31 12:18:22 +0100

    FO-291 Refaktorerer test

--- src/svar/svar-duck.test.tsx
+++ src/svar/svar-duck.test.tsx
@@ -3,18 +3,23 @@ import spm from '../sporsmal/sporsmal-alle';
 
 describe('svar-duck', () => {
     describe('erPaVeiBakover', () => {
-        it('skal finne ut om brukeren er på vei bakover i spørsmålrekkefølgen', () => {
-            const ids = spm.map(s => s.id);
 
-            const spm1 = ids[0];
-            const spm2 = ids[4];
-            const spm3 = ids[7];
-            const spm4 = ids[9];
+        const ids = spm.map(s => s.id);
 
-            expect(erPaVeiBakover(spm2, spm3)).toBe(false);
-            expect(erPaVeiBakover(spm4, spm2)).toBe(true);
+        const spm1 = ids[0];
+        const spm5 = ids[4];
+        const spm8 = ids[7];
+        const spm10 = ids[9];
+
+        it('bruker er på vei bakover', () => {
+            expect(erPaVeiBakover(spm5, spm8)).toBe(false);
+        });
+        it('bruker er på vei forover', () => {
+            expect(erPaVeiBakover(spm10, spm5)).toBe(true);
+        });
+        it('bruker er på gjeldende spørsmål', () => {
             expect(erPaVeiBakover(spm1, spm1)).toBe(false);
-            expect(erPaVeiBakover(spm4, spm4)).toBe(false);
+            expect(erPaVeiBakover(spm10, spm10)).toBe(false);
         });
     });
 });
